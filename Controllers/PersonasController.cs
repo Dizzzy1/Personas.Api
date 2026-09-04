57264814b9bace7bb695eb96115b700b0fd6c7bd
Ronald Hache <mya@RONALD>
2026-09-04 16:24:24 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/PersonasController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Personas.Api.Models;
+
+namespace Personas.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonasController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public PersonasController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearPersona(Persona persona)
+        {
+            string connectionString =
+                _configuration.GetConnectionString("DefaultConnection");
+
+            using var connection = new SqlConnection(connectionString);
+
+            string sql = @"
+                INSERT INTO Personas (Nombre, Apellido, Edad)
+                VALUES (@Nombre, @Apellido, @Edad)";
+
+            await connection.ExecuteAsync(sql, persona);
+
+            return Ok("Persona guardada correctamente");
+        }
+    }
+}
